9ed3256aca388400b6033642dbc7f28711da7561
Vladislav Green <vladzel@gmail.com>
2023-03-07 15:54:25 +0300

    Initial Commit

---
+++ Artist/ArtistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistApp.swift
+//  Artist
+//
+//  Created by Vladislav Green on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ArtistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
